aea06ece1159a9f7dc679bfeaaf63bf308178856
adub20018 <alex.w.malone128@gmail.com>
2023-05-13 17:53:43 +1200

    set max_tokens to 100

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -33,7 +33,7 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: generatePrompt(inputRapper, userInput),
       temperature: 0.6,
-      // max_tokens: 300,
+      max_tokens: 100,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch (error) {
